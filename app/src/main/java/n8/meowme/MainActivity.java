6ddc5a569f65edf001ae278fcf5d117e187243df
Guest User <Guest@epicodus-81.local>
2016-11-18 10:11:20 -0800

    add button, routing user to userInputActivity.class

--- app/src/main/java/n8/meowme/MainActivity.java
+++ app/src/main/java/n8/meowme/MainActivity.java
@@ -1,18 +1,21 @@
 package n8.meowme;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     @Bind(R.id.mainActivityTextView) TextView mMainActivityTextView;
-//
-//    private TextView mMainActivityTextView;
+    @Bind(R.id.mainActivityButton) Button mMainActivityButton;
 
 
 
@@ -22,8 +25,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-//        mMainActivityTextView = (TextView) findViewById(R.id.mainActivityTextView);
+        mMainActivityButton.setOnClickListener(this);
+
+
         Typeface mainFontTextView = Typeface.createFromAsset(getAssets(), "fonts/anomalyDemo.ttf");
             mMainActivityTextView.setTypeface(mainFontTextView);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mMainActivityButton){
+//           Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(MainActivity.this, UserInputActivity.class);
+            startActivity(intent);
+        }
     }
 }
