d67ab7388f1d740f7f8ae0c6ef8b6771b6748df3
Guest User <Guest@epicodus-81.local>
2016-11-18 09:46:36 -0800

    add styling to user input activity page and add image.

--- app/src/main/java/n8/meowme/MainActivity.java
+++ app/src/main/java/n8/meowme/MainActivity.java
@@ -1,13 +1,29 @@
 package n8.meowme;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.mainActivityTextView) TextView mMainActivityTextView;
+//
+//    private TextView mMainActivityTextView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+//        mMainActivityTextView = (TextView) findViewById(R.id.mainActivityTextView);
+        Typeface mainFontTextView = Typeface.createFromAsset(getAssets(), "fonts/anomalyDemo.ttf");
+            mMainActivityTextView.setTypeface(mainFontTextView);
     }
 }
