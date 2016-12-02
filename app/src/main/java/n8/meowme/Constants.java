ccf647d3717ea5477440846b5c59c9f98d70738f
Nhat Hoang <hoangnh092185@gmail.com>
2016-12-02 10:13:55 -0800

    add constants petfinder parameter.

---
+++ app/src/main/java/n8/meowme/Constants.java
@@ -0,0 +1,17 @@
+package n8.meowme;
+
+/**
+ * Created by Guest on 12/2/16.
+ */
+public class Constants {
+    public static final String PETFINDER_KEY = BuildConfig.PETFINDER_KEY;
+    public static final String PETFINDER_PET_FIND_URL = "http://api.petfinder.com/pet.find?";
+    public static final String PETFINDER_PET_GET_URL = "http://api.petfinder.com/pet.get?";
+    public static final String PETFINDER_PET_GETRANDOM_URL = "http://api.petfinder.com/pet.getRandom?";
+    public static final String PETFINDER_ANIMAL_PARAMETER = "animal";
+    public static final String PETFINDER_LOCATION_PARAMETER = "location";
+    public static final String PETFINDER_OUTPUT_PARAMETER = "output";
+    public static final String PETFINDER_COUNT_PARAMETER = "count";
+    public static final String PETFINDER_FORMAT_PARAMETER = "format";
+
+}
